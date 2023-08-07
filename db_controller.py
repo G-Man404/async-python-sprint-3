fdbe48456ecd97e5dba45c7554aea8704380ff89
gman <anisimovf94@gmail.com>
2023-08-07 20:43:08 +0300

    v 0.6.5

--- db_controller.py
+++ db_controller.py
@@ -108,20 +108,23 @@ def give_received_message_ptp(user, sender):
     return new_message
 
 
-def give_all_message_everyone(user):
-    messages = Message.select().where(Message.message_type == "everyone")
-    return messages[-20:]
-
-
-def give_all_message_ptp(user, sender):
-    sender_object = Users.select().where(Users.login == sender)
-    messages = Message.select().where(Message.message_type == "ptp", Message.sender == sender_object,
-                                      Message.recipient == user)
-    return messages
+# def give_all_message_everyone(user):
+#     messages = Message.select().where(Message.message_type == "everyone")
+#     return messages[-20:]
+#
+#
+# def give_all_message_ptp(user, sender):
+#     sender_object = Users.select().where(Users.login == sender)
+#     messages = Message.select().where(Message.message_type == "ptp", Message.sender == sender_object,
+#                                       Message.recipient == user)
+#     return messages
 
 
 def give_new_message_everyone(user, last_call_time):
     messages = Message.select().where(Message.message_type == "everyone", Message.sending_time > last_call_time)
+    for message in messages:
+        viewed_message = Views.create(message=message, user=user)
+        viewed_message.save()
     return messages
 
 
@@ -129,6 +132,9 @@ def give_new_message_ptp(user, sender, last_call_time):
     sender_object = Users.select().where(Users.login == sender)
     messages = Message.select().where(Message.message_type == "ptp", Message.sender == sender_object,
                                       Message.recipient == user, Message.sending_time > last_call_time)
+    for message in messages:
+        viewed_message = Views.create(message=message, user=user)
+        viewed_message.save()
     return messages
 
 
